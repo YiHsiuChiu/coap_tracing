68bd2efda89cfa3da322565ba0f7214f104786c6
Lu-weiting <runeeld23@gmail.com>
2025-03-11 01:06:55 +0800

    feat: dockerize

---
+++ dockerize/load-test/k6.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { randomBytes } from 'k6/crypto';
+
+
+export default function() {
+
+  const traceId = randomBytes(16).hex(); 
+  const spanId = randomBytes(8).hex();
+  const traceHeader = `00-${traceId}-${spanId}-01`;
+
+  const gatewayHost = __ENV.GATEWAY_HOST;
+  const gatewayPort = __ENV.GATEWAY_PORT;
+  const url = `http://${gatewayHost}:${gatewayPort}/iot-test`;
+
+  let res = http.get(url, {
+    headers: {
+      'traceparent': traceHeader,
+      'Content-Type': 'application/json'
+    }
+  });
+  check(res, { 'status 200': (r) => r.status === 200 });
+
+  sleep(1);
+}
