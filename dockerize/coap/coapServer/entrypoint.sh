c4ff8fc62aee5362eef652c795ce32438485ced4
Lu-weiting <runeeld23@gmail.com>
2025-03-11 22:38:54 +0800

    feat: dockerize

--- dockerize/coap/coapServer/entrypoint.sh
+++ dockerize/coap/coapServer/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 
-# 1) 設定 IP (假設在 docker-compose 指定 iot-device-a IP=172.30.0.10)
-ip route add default via 172.30.0.1 dev eth0
+ip route add default via 172.30.0.2 dev eth0
 
 # egress TBF for 雙向
 tc qdisc del dev eth0 root 2>/dev/null || true
