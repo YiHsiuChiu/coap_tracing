08bc8e5eab0d9d2af0a4988097e89814609d7e9d
Lu-weiting <runeeld23@gmail.com>
2025-03-11 22:59:01 +0800

    feat: dockerize

--- dockerize/coap/gateway/entrypoint.sh
+++ dockerize/coap/gateway/entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 # 2) 在 ethx (連到 iot-net) 介面做 TBF
-tc qdisc del dev eth1 root 2>/dev/null || true
-tc qdisc add dev eth1 root tbf rate 250kbit burst 32kbit latency 400ms
+tc qdisc del dev eth0 root 2>/dev/null || true
+tc qdisc add dev eth0 root tbf rate 250kbit burst 32kbit latency 400ms
 
 # 3) 執行原本 gateway 的程式邏輯
 exec node gateway.js
