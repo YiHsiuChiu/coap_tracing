5a2643cd738cb179f3e13264e78fad3a58c6ba4a
Lu-weiting <runeeld23@gmail.com>
2025-03-20 22:18:01 +0800

    test

--- dockerize/coap-opt/coapServer/entrypoint.sh
+++ dockerize/coap-opt/coapServer/entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 # 刪除 Docker 預設路由
 ip route del 172.31.0.0/24 dev eth0 || true
 
+# 2) 加入 host route，讓系統知道要怎麼抵達 172.31.0.2 這個 IP
+ip route add 172.31.0.2 dev eth0
+
+# 3) 再來才能指定 default gateway 為 172.31.0.2
 ip route add default via 172.31.0.2 dev eth0
 
 # egress TBF for 雙向
